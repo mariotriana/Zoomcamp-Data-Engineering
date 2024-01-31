e045868af43d3fbf3abaad798c8f25052d0163b5
mariotriana <89442960+mariotriana@users.noreply.github.com>
2024-01-31 14:25:18 -0500

    Add files via upload

---
+++ first_one.py
@@ -0,0 +1,13 @@
+"""if __name__ == '__main__':
+    
+    print("Hello World")"""
+ 
+import sys   
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+print (f'job finished succesfuly for day=f{day}')
